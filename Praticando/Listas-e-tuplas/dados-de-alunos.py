7d468a1e1996d6a89220cf0088f5f5bf25a5c31c
Renan Ferreira <renanferreirads@hotmail.com>
2025-05-04 17:10:48 -0300

    Atualiza a documentação do programa para descrever a entrada e saída esperadas dos dados dos alunos

--- Praticando/Listas-e-tuplas/dados-de-alunos.py
+++ Praticando/Listas-e-tuplas/dados-de-alunos.py
@@ -1,29 +1,6 @@
 '''
-Uma escola está organizando os dados dos alunos para criar um relatório resumido. Cada aluno tem seus dados registrados em uma única entrada, incluindo nome, idade e nota final no semestre. Esses dados devem ser exibidos separadamente para cada aluno no formato abaixo:
-
-Aluno: Nome
-Idade: Idade
-Nota: Nota
-Copiar código
-Ajude a escola a desenvolver um programa que registre as informações dos alunos, organize os dados e exiba um relatório detalhado com as informações separadamente.
-
-Exemplo de Entrada:
-
-Digite os dados do aluno no formato Nome, Idade, Nota separados por vírgula: João, 16, 8.5, Maria, 17, 9.2, Pedro, 15, 7.8
-Copiar código
-Saída esperada:
-
-Aluno: João
-Idade: 16
-Nota: 8.5
-
-Aluno: Maria
-Idade: 17
-Nota: 9.2
-
-Aluno: Pedro
-Idade: 15
-Nota: 7.8
+Esse programa solicita ao usuário que insira os dados de alunos (nome, idade e nota) separados por vírgula.
+Em seguida, ele processa esses dados em uma lista e exibe um relatório formatado com as informações de cada aluno.
 '''
 
 import os
