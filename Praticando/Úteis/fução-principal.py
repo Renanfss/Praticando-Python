d9932679e832fc5e2f08a036898fe69979ec2fa6
Renan Ferreira <renanferreirads@hotmail.com>
2025-05-04 14:47:06 -0300

    Definição da função princiapal

--- Praticando/Úteis/fução-principal.py
+++ Praticando/Úteis/fução-principal.py
@@ -2,6 +2,10 @@
     Essa função é um exemplo de como criar uma função principal em Python, útil para organizar e facilitar o teste dos scripts.
     Ela chama a função funcao() que executa um código específico, e depois pergunta ao usuário se deseja voltar ao menu principal ou sair do programa.
 '''
+import os
+
+def funcao():
+    #Código da função
 
 def main():
     os.system('cls')
