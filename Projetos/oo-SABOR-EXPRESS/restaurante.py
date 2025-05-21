d0a705b356de206edc3ce955db243b47366b6556
Renan Ferreira <renanferreirads@hotmail.com>
2025-05-21 18:03:56 -0300

    Refatora a classe Restaurante para inicializar atributos no construtor e implementar métodos de listagem e representação

--- Projetos/oo-SABOR-EXPRESS/restaurante.py
+++ Projetos/oo-SABOR-EXPRESS/restaurante.py
@@ -1,14 +1,25 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante() #Criação do objeto
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self._ativo = False #Ao colocar _ na frente do atributo, estamos indicando que ele é privado
+        Restaurante.restaurantes.append(self) 
+        
+    def __str__(self):     # O metodo __str__ é chamado quando usamos print() no objeto
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | Status')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
+    
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
 
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza Express", "Italiano")
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print()
+Restaurante.listar_restaurantes()
